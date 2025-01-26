b54d0bc23eadbeea6a67a571061a7dea1acb39dd
Md. Kamruzzaman <4kamruzzaman@gmail.com>
2025-01-26 10:11:34 -0500

    first commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,10 @@
+// filepath: tailwind.config.js
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}", // Adjust the paths according to your project structure
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
